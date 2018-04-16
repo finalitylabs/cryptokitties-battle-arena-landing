6f088f445247a202f42b3569810169551d67f131
William Vatcher <vatcher@Williams-MBP.hsd1.ca.comcast.net>
2018-04-16 12:29:16 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+          <div className='space'></div>
+          <div className='App-logo'></div>
+          <div className='ground'></div>
+          <div className='content-main'></div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
