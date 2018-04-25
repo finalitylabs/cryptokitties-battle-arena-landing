61f157ce653a460e0780f217cc26a37d4a0a56ff
Will Vatcher <will.vatcher@gmail.com>
2018-04-25 15:33:37 -0700

    Layout changes

--- src/components/ContentBox/ContentBox.style.js
+++ src/components/ContentBox/ContentBox.style.js
@@ -6,24 +6,24 @@ const fadeInAnimation = keyframes`${fadeIn}`;
 export const ContentBoxStyled = styled.div`
     position: absolute;
     border-radius: 10px;
-    padding-left: 50px;
-    padding-right: 50px;
+    padding-left: 40px;
+    padding-right: 40px;
     color: #000;
     border: 5px solid black;
     box-shadow: 2px 2px 20px 5px rgba(0,0,0,0.2);
-    top: 7em;
+    top: 8em;
     font-size: 1.4em;
     align-self: center;
     height: 65vh;
-    width: 50vw;
+    width: 45vw;
     z-index: 888;
     background: white;
     overflow: scroll;
     animation: 2s ${fadeInAnimation};
 
     p {
-        margin-top: 3em;
-        padding: 30px;
+        margin-top: 1.5em;
+        padding: 20px;
         text-align: left;
 
     }
